5421f4a6a88423f97acb7ce90bdf1c1d9e4cfb52
otabekoff <sadiridinovotabek@gmail.com>
2026-05-18 12:47:54 +0500

    fix: deployment headers

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['127.0.0.1']
+  allowedDevOrigins: ["127.0.0.1"],
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "browsing-topics=(), interest-cohort=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
